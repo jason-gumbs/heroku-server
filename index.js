57d81385f68db571b1f2c718542f9625ce30b771
wxg8ihv <wilfred_gumbs@homedepot.com>
2019-09-07 20:01:29 -0400

    done

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var session = require("express-session");
+const passport = require("passport");
+var cors = require("cors");
+const herokuStrategy = require("passport-heroku").Strategy;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+let prof = null;
+
+var HEROKU_CLIENT_ID = "220d8473-396e-4a1f-be88-e14d2761677e";
+var HEROKU_CLIENT_SECRET = "a3097cad-1f32-4dc9-898e-fcf079f64ea4";
+const PORT = process.env.PORT || 3000;
+
+passport.use(
+  new herokuStrategy(
+    {
+      clientID: HEROKU_CLIENT_ID,
+      clientSecret: HEROKU_CLIENT_SECRET,
+      callbackURL: "http://127.0.0.1:3000/auth/heroku/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      prof = profile;
+      return done(null, profile);
+    }
+  )
+);
+
+//authenticate to heroku
+app.get("/auth/heroku", passport.authenticate("heroku"));
+//handle response
+app.get(
+  "/auth/heroku/callback",
+  passport.authenticate("heroku", { failureRedirect: "/error" }),
+  function(req, res) {
+    res.redirect("/success");
+  }
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+app.get("/success", function(req, res) {
+  res.send(prof);
+});
+
+app.get("/error", function(req, res) {
+  res.send("error logged in");
+});
+
+app.get("/", function(req, res) {
+  res.send("success logged in");
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
