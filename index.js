bd5184c6698d3d3b7102fe124ec7a53dfad16f90
wxg8ihv <wilfred_gumbs@homedepot.com>
2019-09-07 23:39:01 -0400

    done

--- index.js
+++ index.js
@@ -3,61 +3,24 @@ const bodyParser = require("body-parser");
 var session = require("express-session");
 const passport = require("passport");
 var cors = require("cors");
+const axios = require("axios");
 const herokuStrategy = require("passport-heroku").Strategy;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use(bodyParser.json());
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(cors());
+
 let prof = null;
 
-var HEROKU_CLIENT_ID = process.env.HEROKU_CLIENT_ID;
+var HEROKU_OAUTH_ID = "5f786f89-6572-447c-938e-e43329e74777";
+var HEROKU_OAUTH_SECRET = "14051aa2-ad87-4ebb-add9-768a3e79b34d";
 
-var HEROKU_CLIENT_SECRET = process.env.HEROKU_CLIENT_SECRET;
-const PORT = process.env.PORT || 3000;
+var HEROKU_CLIENT_ID =
+  process.env.HEROKU_CLIENT_ID || "8063f075-1e7f-4ff7-a23f-f50df3f45128";
 
-passport.use(
-  new herokuStrategy(
-    {
-      clientID: HEROKU_CLIENT_ID,
-      clientSecret: HEROKU_CLIENT_SECRET,
-      callbackURL: "https://passport-upwork.herokuapp.com/auth/heroku/callback"
-    },
-    function(accessToken, refreshToken, profile, done) {
-      prof = profile;
-      return done(null, profile);
-    }
-  )
-);
-
-//authenticate to heroku
-
-app.get("/auth/heroku", passport.authenticate("heroku"));
-//handle response
-app.get(
-  "/auth/heroku/callback",
-  passport.authenticate("heroku", { failureRedirect: "/error" }),
-  function(req, res) {
-    res.redirect("/success");
-  }
-);
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
+var HEROKU_CLIENT_SECRET =
+  process.env.HEROKU_CLIENT_SECRET || "a3097cad-1f32-4dc9-898e-fcf079f64ea4";
+const PORT = process.env.PORT || 5000;
 
 app.get("/success", function(req, res) {
   res.send(prof);
@@ -67,8 +30,38 @@ app.get("/error", function(req, res) {
   res.send("error logged in");
 });
 
-app.get("/", function(req, res) {
-  res.send("success loggedzxzczxc in");
+app.get("/auth", function(req, res) {
+  // res.send("success loggedzxzczxc in");
+  res.send(
+    `https://id.heroku.com/oauth/authorize?client_id=${HEROKU_OAUTH_ID}&response_type=code&scope=identity&state=mytestapp`
+  );
 });
 
+app.post("/token", function(req, res) {
+  var token = req.body.token || "edd2458f-2e10-4a7b-afac-0c0e77a6e520";
+  axios
+    .post(
+      `https://id.heroku.com/oauth/token?grant_type=authorization_code&code=${token}&client_secret=${HEROKU_OAUTH_SECRET}`
+    )
+    .then(function(response) {
+      res.send(response);
+    })
+    .catch(function(error) {
+      res.send(error);
+    });
+});
+
+app.post("/refresh", function(req, res) {
+  var token = req.body.token;
+  axios
+    .post(
+      `https://id.heroku.com/oauth/token?grant_type=refresh_token&refresh_token=${token}&client_secret=${HEROKU_OAUTH_SECRET}`
+    )
+    .then(function(response) {
+      res.send(response);
+    })
+    .catch(function(error) {
+      res.send(error);
+    });
+});
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
